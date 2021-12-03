32f8945088523f2e5b7f5f74f93949dff1cff9fc
Ghanshyam Vaishnav <80735317+vaishnav27@users.noreply.github.com>
2021-12-03 16:23:04 +0530

    Create 344. Reverse String.cpp

---
+++ 344. Reverse String.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        
+        //  int start =0, end = s.size()-1;
+        // while(start<=end){
+        //     swap(s[start],s[end]);
+        //     start++;
+        //     end--;
+        // }
+        reverse(s.begin(),s.end());
+    }
+};
