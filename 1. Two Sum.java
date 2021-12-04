474bce22992aec5d0ada4c7d49d49cf07dbada7a
Ghanshyam Vaishnav <80735317+vaishnav27@users.noreply.github.com>
2021-12-04 17:49:34 +0530

    Create 1. Two Sum.java

---
+++ 1. Two Sum.java
@@ -0,0 +1,38 @@
+// Time Complexity 0(n)
+     class Soution{
+        HashMap<Integer,Integer>map = new HashMap<>();
+        
+        int[] res = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            int x=target-nums[i];
+            if (map.containsKey(x)){
+                res[1]=i;
+                res[0]=map.get(x);
+        }
+            map.put(nums[i],i);
+        }
+         return res;
+        
+    }
+}
+
+
+// Time Complexity 0(n^2)
+
+  class Solution {
+    public int[] twoSum(int[] nums, int target) {
+         int[] arr = new int[2];
+         for (int i = 0; i < nums.length; i++) {
+             for (int j = i + 1; j < nums.length; j++) {
+                 if (target == nums[i] + nums[j]) {
+                     arr[0] = i;
+                   arr[1] = j;
+                 }
+             }
+         }
+        return arr;
+    }
+}
+
+      
+ 
