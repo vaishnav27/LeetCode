65f40e3cd395328f11b6aad8d58d6955892c1628
Ghanshyam Vaishnav <80735317+vaishnav27@users.noreply.github.com>
2021-12-16 21:49:28 +0530

    Create 700. Search in a Binary Search Tree.java

---
+++ 700. Search in a Binary Search Tree.java
@@ -0,0 +1,16 @@
+
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root==null)
+            return root;
+        
+        if(root.val==val)
+            return root;
+        else
+            if(val<root.val)
+                return searchBST(root.left,val);
+            else
+                return searchBST(root.right,val);
+        
+    }
+}
