9e75d5eb67258668cc691ad0b4ed0a60e1b23390
Ghanshyam Vaishnav <80735317+vaishnav27@users.noreply.github.com>
2021-12-28 15:43:06 +0530

    Update 1920. Build Array from Permutation.java

--- 1920. Build Array from Permutation.java
+++ 1920. Build Array from Permutation.java
@@ -13,3 +13,17 @@ class Solution {
         return nums;
     }
 }
+
+// Simple and Clear Solution
+
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int[] res = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            res[i] = nums[nums[i]];
+
+        }
+        return res;
+         
+    }
+}
