6f52ab9e364878505daab042dd16d9f6b52c42e9
Ghanshyam Vaishnav <80735317+vaishnav27@users.noreply.github.com>
2022-01-04 07:48:11 +0530

    Create 2108. Find First Palindromic String in the Array.java

---
+++ 2108. Find First Palindromic String in the Array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String firstPalindrome(String[] words) {
+
+        for (String word : words) {
+            if (isPalindrome(word))
+                return word;
+        }
+        return "";
+    }
+
+    public boolean isPalindrome(String word) {
+        int left = 0;
+        int right = word.length() - 1;
+        while (left < right) {
+            if (word.charAt(left++) != word.charAt(right--))
+                return false;
+
+          
+        } 
+        return true;
+    }
+}
+
