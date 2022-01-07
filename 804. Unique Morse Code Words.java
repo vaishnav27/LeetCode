2123c360d737306534407876b30c55dbd9ed87bf
Ghanshyam Vaishnav <80735317+vaishnav27@users.noreply.github.com>
2022-01-07 21:36:02 +0530

    Create 804. Unique Morse Code Words.java

---
+++ 804. Unique Morse Code Words.java
@@ -0,0 +1,21 @@
+
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] Morse = new String[] { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-",
+                ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--",
+                "--.." };
+
+        Set<String> seen = new HashSet<>();
+
+        for (String word : words) {
+
+            StringBuilder sb = new StringBuilder();
+            for (char ch : word.toCharArray()) {
+                sb.append(Morse[ch - 'a']);
+            }
+            seen.add(sb.toString());
+        }
+        return seen.size();
+    }
+
+}
