b12589e1de0d1041c7a535070e2e5e50dc6355d8
Ghanshyam Vaishnav <80735317+vaishnav27@users.noreply.github.com>
2022-01-28 17:47:54 +0530

    Create 1637. Widest Vertical Area Between Two Points Containing No Points.java

---
+++ 1637. Widest Vertical Area Between Two Points Containing No Points.java
@@ -0,0 +1,17 @@
+
+class Solution {
+    public int maxWidthOfVerticalArea(int[][] points) {
+        int[] arr = new int[points.length];
+
+        for (int i = 0; i < points.length; i++) {
+            arr[i] = points[i][0];
+        }
+        Arrays.sort(arr);
+        int mx = 0;
+        for (int i = 1; i < arr.length; i++) {
+            mx = Math.max(mx, (arr[i] - arr[i - 1]));
+        }
+        return mx;
+
+    }
+}
