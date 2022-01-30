5f43db19f021d3cd68861ac553f86eef698a1dac
Ghanshyam Vaishnav <80735317+vaishnav27@users.noreply.github.com>
2022-01-30 17:41:33 +0530

    Update 1409. Queries on a Permutation With Key.java

--- 1409. Queries on a Permutation With Key.java
+++ 1409. Queries on a Permutation With Key.java
@@ -26,3 +26,31 @@ class Solution {
         return ans;
     }
 }
+
+// Using Linkedlist
+
+import java.util.LinkedList;
+
+class Solution {
+    public int[] processQueries(int[] queries, int m) {
+        LinkedList<Integer> p = new LinkedList<>();
+
+        for (int i = 1; i <= m; i++) {
+            p.add(i);
+        }
+        int[] ans = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            int q = queries[i];
+
+            int idx = p.indexOf(q);
+            int val = p.get(idx);
+
+            p.remove(idx);
+            p.addFirst(val);
+
+            ans[i] = idx;
+        }
+
+        return ans;
+    }
+}
