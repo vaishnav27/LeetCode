fa5b9afa1c43645c48f6caf64f9d6f0d1b58efee
Ghanshyam Vaishnav <80735317+vaishnav27@users.noreply.github.com>
2022-02-16 21:40:59 +0530

    Create 24. Swap Nodes in Pairs.java

---
+++ 24. Swap Nodes in Pairs.java
@@ -0,0 +1,15 @@
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode temp = head.next;
+        
+        head.next = swapPairs(head.next.next);
+
+        temp.next = head;
+
+        return temp;
+
+    }
+}
