8d635febdf937f4a114e9098df86dc6e42875e5c
Ghanshyam Vaishnav <80735317+vaishnav27@users.noreply.github.com>
2022-02-18 21:10:01 +0530

    Create 402. Remove K Digits.java

---
+++ 402. Remove K Digits.java
@@ -0,0 +1,41 @@
+
+class Solution {
+    public String removeKdigits(String num, int k) {
+
+        int n = num.length();
+
+        if (n == k)
+            return "0";
+
+        Stack<Character> stack = new Stack<>();
+
+        int i = 0;
+
+        while (i < n) {
+            while (!stack.isEmpty() && k > 0 && stack.peek() > num.charAt(i)) {
+                stack.pop();
+                k--;
+            }
+            stack.push(num.charAt(i));
+            i++;
+        }
+
+        while (k > 0) {
+            stack.pop();
+            k--;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+
+        }
+        sb.reverse();
+
+        while (sb.length() > 1 && sb.charAt(0) == '0') {
+            sb.deleteCharAt(0);
+        }
+        return sb.toString();
+    }
+}
