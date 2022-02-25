c32314ebc89d9acf93ac900661efdaef1e8598d7
Ghanshyam Vaishnav <80735317+vaishnav27@users.noreply.github.com>
2022-02-25 20:28:14 +0530

    Create 165. Compare Version Numbers.java

---
+++ 165. Compare Version Numbers.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int compareVersion(String version1, String version2) {
+
+        String[] s1 = version1.split("\\.");
+        String[] s2 = version2.split("\\.");
+        
+        int maxlen = Math.max(s1.length, s2.length);
+
+        for (int i = 0; i < maxlen; i++) {
+            int v1 = i < s1.length ? Integer.parseInt(s1[i]) : 0;
+            int v2 = i < s2.length ? Integer.parseInt(s2[i]) : 0;
+
+            if (v1 > v2)
+                return 1;
+            else if (v1 < v2)
+                return -1;
+        }
+        return 0;
+
+    }
+}
