f88e8854a639364c0eb32672d0c8c2c05483ae72
Ghanshyam Vaishnav <80735317+vaishnav27@users.noreply.github.com>
2022-04-02 22:34:14 +0530

    Create 503. Next Greater Element II.java

---
+++ 503. Next Greater Element II.java
@@ -0,0 +1,24 @@
+
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        Stack<Integer> st = new Stack<>();
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        for (int i = 2 * n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && st.peek() <= nums[i % n]) {
+                st.pop();
+            }
+            if (i < n) {
+                if (!st.isEmpty())
+                    ans[i] = st.peek();
+                else
+                    ans[i] = -1;
+            }
+            st.push(nums[i % n]);
+
+        }
+        return ans;
+
+    }
+}
