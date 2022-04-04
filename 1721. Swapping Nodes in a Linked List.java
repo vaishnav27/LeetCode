7d22fb71923bdbae75908a26bbb976730672fbac
Ghanshyam Vaishnav <80735317+vaishnav27@users.noreply.github.com>
2022-04-04 22:31:49 +0530

    Create 1721. Swapping Nodes in a Linked List.java

---
+++ 1721. Swapping Nodes in a Linked List.java
@@ -0,0 +1,26 @@
+
+class Solution {
+    public ListNode swapNodes(ListNode head, int k) {
+       ListNode slow=head;
+       ListNode fast=head;
+       ListNode first=head,second=head;
+
+       for (int i = 0; i < k-1; i++) {
+           fast=fast.next;
+       }
+       first=fast;
+
+       while (fast.next!=null) {
+           slow=slow.next;
+           fast=fast.next;
+       }
+       second=slow;
+
+       int temp=first.val;
+       first.val=second.val;
+       second.val=temp;
+
+       return  head;
+
+    }
+}
