0dabad73c539aa4c512b6bab908e0fe3b72ebea3
Ghanshyam Vaishnav <80735317+vaishnav27@users.noreply.github.com>
2022-04-25 22:47:02 +0530

    Create 2248. Intersection of Multiple Arrays.java

---
+++ 2248. Intersection of Multiple Arrays.java
@@ -0,0 +1,19 @@
+
+class Solution {
+    public List<Integer> intersection(int[][] nums) {
+        int[] freq = new int[1001];
+
+        for (int[] num : nums) {
+            for (int i : num) {
+                freq[i]++;
+            }
+        }
+        List<Integer> ans = new ArrayList<>();
+
+        for (int i = 1; i < 1001; i++) {
+            if (freq[i] == nums.length)
+                ans.add(i);
+        }
+        return ans;
+    }
+}
