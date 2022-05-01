1d4dc6ad926551850f3c1dfc33b8819441a20a24
Ghanshyam Vaishnav <80735317+vaishnav27@users.noreply.github.com>
2022-05-01 22:51:30 +0530

    Create 2261. K Divisible Elements Subarrays.java

---
+++ 2261. K Divisible Elements Subarrays.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int countDistinct(int[] nums, int k, int p) {
+        Set<String> set = new HashSet<>();
+       
+        for (int i = 0; i < nums.length; i++) { 
+            int count = 0;
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int j = i; j < nums.length; j++) {
+                if (nums[j] % p == 0)
+                    count++;
+                if (count > k)
+                    break;
+                sb.append(nums[j] + "_");
+                set.add(sb.toString());
+            }
+            
+        }
+        return set.size();
+    }
+}
