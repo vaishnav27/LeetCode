677e18b91983a9dcc3875484036a9e5afd8bc0b4
Ghanshyam Vaishnav <80735317+vaishnav27@users.noreply.github.com>
2022-07-23 22:05:29 +0530

    Create 2348. Number of Zero-Filled Subarrays.java

---
+++ 2348. Number of Zero-Filled Subarrays.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long zeroFilledSubarray(int[] nums) {
+        long count = 0,ans=0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                count++;
+            } else {
+                ans += (count * (count + 1)) / 2;
+                count = 0;
+            }
+        }
+        ans += (count * (count + 1)) / 2;
+        return ans;
+    }
+}
